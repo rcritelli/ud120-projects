afa11f7049c01e102b2065c8799b2b67ffeebae8
Renan Critelli <dm.renan@gmail.com>
2019-02-23 19:10:56 -0300

    add py3.6 compat to validate_poy.py, also using sort_keys argument as instructed by Udacity

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 """
     Starter code for the validation mini-project.
     The first step toward building your POI identifier!
@@ -15,18 +14,16 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys='../tools/python2_lesson13_keys.pkl') # as Udacity instructed
 labels, features = targetFeatureSplit(data)
 
 
-
 ### it's all yours from here forward!  
-
 
